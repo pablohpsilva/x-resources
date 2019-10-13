4903725d5ca7b2a0774bb9eb3c6dbd5732efc037
pablohenrique <pablo@softbox.com.br>
2019-10-13 07:34:57 +0200

    chore(src): moved files

---
+++ src/curry.test.js
@@ -0,0 +1,15 @@
+import "core-js/stable";
+import "regenerator-runtime/runtime";
+import curry from './curry'
+
+const curryFn = curry((a, b, c) => a + b + c)
+
+test('testing curry returning functions', () => {
+  expect(typeof curryFn(1)).toBe('function')
+  expect(typeof curryFn(1)(2)).toBe('function')
+  expect(curryFn(1)(2)(3)).toBe(6)
+})
+
+test('testing curry returning straight value', () => {
+  expect(curryFn(1, 2, 3)).toBe(6)
+})
