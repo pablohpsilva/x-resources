cc626436436b9c31e84904b2cc45815c47285eea
pablohenrique <pablo@softbox.com.br>
2019-12-30 19:49:05 +0100

    chore: updated tests

--- index.test.js
+++ index.test.js
@@ -1,53 +1,64 @@
 import "core-js/stable";
 import "regenerator-runtime/runtime";
-import { resources } from './dist/axios-resources'
+import { resources } from "./dist/index";
 
 const simpleActions = {
-  fetchUsers: { method: 'GET', url: '/users' },
-  fetchSpecificUser: { method: 'GET', url: '/users/:username' },
-  fetchSpecificUserQueryParam: { method: 'DELETE', url: '/users/:username?id=:id' },
-}
+  fetchUsers: { method: "GET", url: "/users" },
+  fetchSpecificUser: { method: "GET", url: "/users/:username" },
+  fetchSpecificUserQueryParam: {
+    method: "DELETE",
+    url: "/users/:username?id=:id"
+  }
+};
 
-const baseURL = 'https://api.github.com'
+const baseURL = "https://api.github.com";
 
-const simpleResource = resources(baseURL, simpleActions)
+const simpleResource = resources(baseURL, simpleActions);
 
-const username = 'pablohpsilva'
+const username = "pablohpsilva";
 
-const id = 2090635
+const id = 2090635;
 
-test('create resource using axios', async () => {
-  expect(Object.keys(simpleResource).length).toBe(9)
-  const resourceCreated = Object.values(simpleResource)
+test("create resource using axios", async () => {
+  expect(Object.keys(simpleResource).length).toBe(9);
+  const resourceCreated = Object.values(simpleResource);
 
-  expect(resourceCreated.every(el => el instanceof Function)).toBe(true)
-})
+  expect(resourceCreated.every(el => el instanceof Function)).toBe(true);
+});
 
-test('fetchUsers from resource', async () => {
-  const response = await simpleResource.fetchUsers()
-    .catch(error => { throw error })
+test("fetchUsers from resource", async () => {
+  const response = await simpleResource.fetchUsers().catch(error => {
+    throw error;
+  });
 
-  const { data } = response
+  const { data } = response;
 
-  expect(data instanceof Array).toBe(true)
-  expect(!!data.length).toBe(true)
+  expect(data instanceof Array).toBe(true);
+  expect(!!data.length).toBe(true);
 
   if (data.length && data[0]) {
-    const user = data[0]
-    expect(Object.prototype.hasOwnProperty.call(user, 'login')).toBe(true)
+    const user = data[0];
+    expect(Object.prototype.hasOwnProperty.call(user, "login")).toBe(true);
   }
 
-  expect(response.request.responseURL).toBe(`${baseURL}/users`)
-})
+  expect(response.request.res.client._httpMessage.res.responseUrl).toBe(
+    `${baseURL}/users`
+  );
+});
 
-test('fetchSpecificUser from resource', async () => {
-  const response = await simpleResource.fetchSpecificUser({ username, id })
-    .catch(error => { throw error })
+test("fetchSpecificUser from resource", async () => {
+  const response = await simpleResource
+    .fetchSpecificUser({ username, id })
+    .catch(error => {
+      throw error;
+    });
 
-  const { data } = response
+  const { data } = response;
 
-  expect(data instanceof Object).toBe(true)
-  expect(data.login).toBe(username)
+  expect(data instanceof Object).toBe(true);
+  expect(data.login).toBe(username);
 
-  expect(response.request.responseURL).toBe(`${baseURL}/users/${username}`)
-})
+  expect(response.request.res.client._httpMessage.res.responseUrl).toBe(
+    `${baseURL}/users/${username}`
+  );
+});
